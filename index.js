540021e2fea6d9b6a40d8f003a612d2b29bbe190
Christian Lopez <lopezchristian1610@gmail.com>
2023-03-11 23:38:32 -0500

    elements added successfully

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById("main");
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Christian is the champion";
+main.remove();
